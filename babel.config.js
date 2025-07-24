1e7d8de4a8493229c322ba70075a2133e879d4c7
dhinesh4668nato <dhinesh@natobotics.com>
2025-07-24 17:49:02 +0530

    Add initial workspace configuration for Sippin project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@hooks': './src/hooks',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    ],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,24 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+    extraNodeModules: {
+        '@components': `${__dirname}/src/omponents`,
+        '@assets': `${__dirname}/src/assets`,
+        '@utils': `${__dirname}/src/utils`,
+        '@hooks': `${__dirname}/src/hooks`,
+        '@screens': `${__dirname}/src/screens`
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
